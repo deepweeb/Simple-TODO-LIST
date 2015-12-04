3cea8414615cd1e3e01aa1060a06b3ee0e626215
deepweeb <T.L.Nguyen@student.tudelft.nl>
2015-12-04 02:29:57 +0100

    no message

--- Client/newjavascript.js
+++ Client/newjavascript.js
@@ -1,14 +1,45 @@
 
+var ToDos = [];//array containing all todo object
+  
+  //change status from "In Process" to "Finished"
+ function Mark(object) {
+   var ClickedToDoID = object.getAttribute("id");
+   console.log(ToDos[0].TodoID +"----"+ ClickedToDoID);
+     for(var i = 0; i < ToDos.length; i++) 
+   {
+       if(ToDos[i].TodoID.toString() === ClickedToDoID)
+       {
+           ToDos[i].Status = "Finished";
+       } 
+     }
+    Print(ToDos);
+}
+
+
+//change remove clicked object
+    function Delete(object) {
+   var ClickedToDoID = object.getAttribute("id");
+   console.log(ToDos[0].TodoID +"----"+ ClickedToDoID);
+     for(var i = 0; i < ToDos.length; i++) 
+   {
+       if(ToDos[i].TodoID.toString() === ClickedToDoID)
+       {
+           ToDos.splice(i,1);
+       }
+    }
+ Print(ToDos);     
+}
+  
+  
 var main = function ()
 {
     "use strict";
-    
-    var ToDos = [];//array containing all todo object
-    
+
 //when the "Add ToDo Button"is clicked
     $('#button1').click(function (event)
     {
         addTodoFromInputBox();
+     
     });
     
 //when the Enter is pushed
@@ -30,25 +61,23 @@ var main = function ()
         {
             soortToDobyDeadline();
         }
-         else if (document.getElementById("Sortby").options[document.getElementById("Sortby").selectedIndex].id === "Progress")
+         else if (document.getElementById("Sortby").options[document.getElementById("Sortby").selectedIndex].id === "PF")
         {
-            soortToDobyProgress();
+            soortPF();
         }
-         else if (document.getElementById("Sortby").options[document.getElementById("Sortby").selectedIndex].id === "Sort")
+         else if (document.getElementById("Sortby").options[document.getElementById("Sortby").selectedIndex].id === "FP")
         {
-            alert("Sort by??");
+            soortFP();
         }
-        else 
-        {
-            alert("not available yet");
-        }
+       
+        $("#Sortby").val('None');
+        
     });
 
 
 
 
 //function to make a Todo and add it.
-
   var addTodoFromInputBox = function ()
     {
         if (($('#inputrange1').val() !== "")
@@ -61,10 +90,9 @@ var main = function ()
                 "Priority": $('#inputrange1').val(),
                 "Task": $('#inputbox2').val(),
                 "Deadline": $('#inputbox3').val(),
-                "Status": "In process "};
+                "Status": "In process"};
                 ToDos.push(new_object);
-                
-                
+ 
  
                 $("#outputbox").prepend("\n\
         <table border=\"0\" id=\"some id\">\n\
@@ -72,12 +100,11 @@ var main = function ()
         <td width=\"300px\" bgcolor=\"#FF8000\" class=\"description\">"+ new_object.Task + " </td> \n\
         <td width=\"90px\" bgcolor=\"#FF8000\" class=\"deadline\">"+ new_object.Deadline +" </td>\n\
 	<td width=\"80px\" bgcolor=\"#FF8000\" class=\"status\">"+ new_object.Status + " </td>\n\
-        <td width=\"28px\" bgcolor=\"#FF8000\" align=\"center\" class=\"change-status-button\"><b>&#x21ba;</b></td>\n\
-	<td width=\"28px\" bgcolor=\"#FF8000\" align=\"center\" class=\"delete-button\"><a style=\"font-family:'Segoe UI Symbol';color:black;font-size:17px;\">&#xe107;</a></td>\n\
+        <td width=\"28px\" bgcolor=\"#FF8000\" align=\"center\" style=\"cursor: pointer;\" onclick=\"Mark(this)\" id="+ new_object.TodoID + ">&#10004;</td>\n\
+	<td width=\"28px\" bgcolor=\"#FF8000\" align=\"center\" style=\"cursor: pointer;\" onclick=\"Delete(this)\" id="+ new_object.TodoID + "><a style=\"font-family:'Segoe UI Symbol';color:black;font-size:17px;\">&#xe107;</a></td>\n\
         </table>");
   
-                
-                                
+                    
                 $('#inputrange1').val("1");
         document.getElementById("rangeText").innerHTML = rangeValues[$('#inputrange1').val()];
         $('#inputbox2').val("");
@@ -85,16 +112,12 @@ var main = function ()
 };
 
 
-  
-
-
-
 
 //function to sort by priority
 var soortToDobyPriority = function ()
 {
     var Priority1 = [];
-    for (var i = 4; i >= 1; i--)
+    for (var i = 1; i <= 4; i++)
     {
         for (var j = 0; j < ToDos.length; j++)
         {
@@ -115,12 +138,34 @@ var soortToDobyDeadline = function ()
     {
      return parseFloat(a.Deadline.replace(/-|\//g, "")) - parseFloat(b.Deadline.replace(/-|\//g, ""));
     });
+   Deadline.reverse();
    Print(Deadline);
 };
 
 
-//function to sort by progress. Wont work now
-var soortToDobyProgress = function ()
+//function to sort by progress PF (In Process - Finished)
+var soortPF = function ()
+{
+    var inProgress =[];
+    var Finished =[];
+     for (var i = 0; i < ToDos.length; i++)
+        {
+            if(ToDos[i].Status === "In process")
+            {inProgress.push(ToDos[i]);}
+
+        }
+     for (var i = 0; i < ToDos.length; i++)
+        {
+            if(ToDos[i].Status === "Finished")
+            {Finished.push(ToDos[i]);}
+        }
+        
+   var sorted = Finished.concat(inProgress);
+   Print(sorted);
+};
+
+//function to sort by progress FP ( Finished - In Process )
+var soortFP = function ()
 {
     var inProgress =[];
     var Finished =[];
@@ -140,23 +185,6 @@ var soortToDobyProgress = function ()
    Print(sorted);
 };
 
-//function to print an array. called in each sorting method.
-var Print = function (A)
-{
-    $("#outputbox").html("");
-    for (var i = 0; i < A.length; i++)
-    {
-        $("#outputbox").append("\n\
-        <table border=\"0\" id=\"some id\">\n\
-        <td width=\"20px\" bgcolor=\"#FF8000\" class=\"priority\"><center>" + A[i].Priority + "</center></td>\n\
-        <td width=\"300px\" bgcolor=\"#FF8000\" class=\"description\">"+ A[i].Task + " </td> \n\
-        <td width=\"90px\" bgcolor=\"#FF8000\" class=\"deadline\">"+ A[i].Deadline +" </td>\n\
-	<td width=\"80px\" bgcolor=\"#FF8000\" class=\"status\">"+ A[i].Status + " </td>\n\
-        <td width=\"28px\" bgcolor=\"#FF8000\" align=\"center\" class=\"change-status-button\"><b>&#x21ba;</b></td>\n\
-	<td width=\"28px\" bgcolor=\"#FF8000\" align=\"center\" class=\"delete-button\"><a style=\"font-family:'Segoe UI Symbol';color:black;font-size:17px;\">&#xe107;</a></td>\n\
-        </table>");
-    }
-};
 
 // for the priority choosing range.
 // define a lookup for what text should be displayed for each value in your range
@@ -177,3 +205,20 @@ $(function () {
     });
 };
         $(document).ready(main);
+//function to print an array. called in each sorting method.
+var Print = function (A)
+{
+    $("#outputbox").html("");
+    for (var i = 0; i < A.length; i++)
+    {
+        $("#outputbox").prepend("\n\
+        <table border=\"0\" id=\"some id\">\n\
+        <td width=\"20px\" bgcolor=\"#FF8000\" class=\"priority\"><center>" + A[i].Priority + "</center></td>\n\
+        <td width=\"300px\" bgcolor=\"#FF8000\" class=\"description\">"+ A[i].Task + " </td> \n\
+        <td width=\"90px\" bgcolor=\"#FF8000\" class=\"deadline\">"+ A[i].Deadline +" </td>\n\
+	<td width=\"80px\" bgcolor=\"#FF8000\" class=\"status\">"+ A[i].Status + " </td>\n\
+        <td width=\"28px\" bgcolor=\"#FF8000\" align=\"center\" style=\"cursor: pointer;\" onclick=\"Mark(this)\" id="+ A[i].TodoID + ">&#10004;</td>\n\
+	<td width=\"28px\" bgcolor=\"#FF8000\" align=\"center\" style=\"cursor: pointer;\" onclick=\"Delete(this)\" id="+ A[i].TodoID + "><a style=\"font-family:'Segoe UI Symbol';color:black;font-size:17px;\">&#xe107;</a></td>\n\
+        </table>");
+    }
+};
